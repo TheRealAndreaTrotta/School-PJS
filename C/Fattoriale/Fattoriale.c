c0d09184199f996089a61429cf6e8d3ddfecd2e0
TheRealProdigy <andreatrotta004@gmail.com>
2023-12-01 23:49:30 +0100

    01 Dicembre 2023

---
+++ C/Fattoriale/Fattoriale.c
@@ -0,0 +1,17 @@
+// calcolare il fattoriale di un numero
+#include <stdio.h>
+
+int main(void){
+    int fattoriale = 1, input = 0;
+
+    printf("Prego, inserisca il numero che desidera calcolare: ");
+    scanf("%d", &input);
+
+    while(input != 0){
+        fattoriale *= input;
+        input --;
+    }
+
+    printf("Il fattoriale risulta:\t%d\n", fattoriale);
+    return 0;   
+}
