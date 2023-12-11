f4c96d125fe3a6ff960cedae5b234f386e54dc60
TheRealProfigy <andreatrotta004@gmail.com>
2023-12-11 11:27:56 +0100

    school

---
+++ Development/Batch/FSG/FSGSchool.bat
@@ -0,0 +1,32 @@
+@echo off 
+echo Creation of the file system...
+
+cd \
+md .\Users\sussato\Desktop\FSGroot
+cd .\Users\sussato\Desktop\FSGroot
+md Documenti,Programmi,Temp
+
+    cd Documenti
+    md File,Foto,Video
+    
+        copy NUL .\file\Diagramma.xls | copy NUL .\file\Lavoro.txt | copy NUL .\file\Program.bat
+        copy NUL .\foto\Camion.gif | copy NUL .\foto\Casa1.png | copy NUL .\foto\Macchina.jpg
+        copy NUL .\video\Ferrari.mp4 | copy NUL .\video\Gladiatore.mov | copy NUL .\video\Film.mp4
+
+    cd ..
+
+    cd Programmi
+    md Office365,Giochi,Altro
+    
+        copy NUL .\office365\Excel.exe | copy NUL .\office365\Outlook.exe | copy NUL .\office365\Word.exe
+        copy NUL .\giochi\Fortnite.exe | copy NUL .\giochi\Minecraft.exe
+        copy NUL .\altro\Photshop.exe | copy NUL .\altro\VScode.exe | copy NUL .\altro\Unity.exe
+
+    cd ..
+
+    copy C:\Users\sussato\Desktop\Development\Development\Batch\FSG\Lorem.txt .\temp
+
+@echo off 
+echo Done!
+
+exit
