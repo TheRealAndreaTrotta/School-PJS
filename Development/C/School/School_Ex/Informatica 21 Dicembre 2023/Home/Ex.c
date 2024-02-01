c9bd99a49533b50cc5d49da55c9bd0eb376e9834
TheRealProdigy <andreatrotta004@gmail.com>
2024-02-01 10:31:23 +0100

    new

--- Development/C/School/School_Ex/Informatica 21 Dicembre 2023/Home/Ex.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int mn[] = {1,2,3};
-
-    printf("%lu\n", sizeof(mn)); //print Byte value (4*3)
-    printf("There is %d numbers in this array!\n",(sizeof(mn)/sizeof(mn[0])));
-    return 0;
-}
