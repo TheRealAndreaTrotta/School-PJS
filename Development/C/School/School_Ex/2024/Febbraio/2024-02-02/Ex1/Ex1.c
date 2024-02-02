2ddbd1efae08a792f655658eab8946618d2e9877
TheRealProdigy <andreatrotta004@gmail.com>
2024-02-02 10:48:59 +0100

    sottostr

--- Development/C/School/School_Ex/2024/Febbraio/2024-02-02/Ex1/Ex1.c
+++ Development/C/School/School_Ex/2024/Febbraio/2024-02-02/Ex1/Ex1.c
@@ -1,4 +1,9 @@
+//sottostringa, verificare se la seconda stringa è presente nella prima
+
 #include <stdio.h>
+#include <string.h>
+
+#define SZ 100
 
 void welcome(void){
     printf("+----------------------------------------------------------+\n");
@@ -8,7 +13,30 @@ void welcome(void){
 
 int main(void){
     welcome();
-     
-    printf("\n+----------------------------------------------------------+\n");
+    
+    char string[SZ], under[SZ];
+    int counter = 0;
+
+    printf("+ Please, write something: ");
+    gets(string);
+
+    printf("+ Under string:  ");
+    gets(under);
+
+    for(int i=0; i<SZ; i++){
+        for(int j=0; j<strlen(under); j++){
+            if(string[i] == under[j]){
+                counter++;
+            }
+        }
+    }
+
+    if(counter == strlen(under)){
+        printf("+ Found it!\n");
+    }
+    else{
+        printf("+ Not found it!\n");
+    }
+    printf("+----------------------------------------------------------+\n");
     return 0;
 }
