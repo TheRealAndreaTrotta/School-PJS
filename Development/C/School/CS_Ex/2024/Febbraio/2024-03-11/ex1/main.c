d76b5509ca2e069f7636838230b3de4a36f585c7
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-03-11 12:52:55 +0100

    matrici

--- Development/C/School/CS_Ex/2024/Febbraio/2024-03-11/ex1/main.c
+++ Development/C/School/CS_Ex/2024/Febbraio/2024-03-11/ex1/main.c
@@ -1,9 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define rows
-#define columns 
+#define rows 4
+#define columns 3
 
-int main(void){
-    int matrix [][];
+#define MAX 9
+#define MIN 1
+
+void random_matx(int matrix[rows][columns]);
+void sum_matx(int matrix_0[rows][columns], int matrix_1[rows][columns]);
+
+int main(void) {
+    srand(time(NULL));
+
+    int matrix_0[rows][columns];
+    int matrix_1[rows][columns];
+
+    puts("Matrix 1:");
+    random_matx(matrix_0);
+
+    puts("");
+
+    puts("Matrix 2:");
+    random_matx(matrix_1);
+
+    puts("Matrix 3:");
+    sum_matx(matrix_0, matrix_1);
+
     return 0;
+}
+
+void random_matx(int matrix[rows][columns]){
+    for (int i = 0; i < rows; i++){
+        printf("\t");
+        for (int j = 0; j < columns; j++){
+            matrix[i][j] = rand() % (MAX - MIN + 1) + MIN;
+            printf("%d  ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void sum_matx(int matrix_0[rows][columns], int matrix_1[rows][columns]){
+    int matrix[rows][columns];
+
+    for (int i = 0; i < rows; i++) {
+        printf("\t");
+        for (int j = 0; j < columns; j++) {
+            matrix[i][j] = matrix_0[i][j] + matrix_1[i][j];
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
 }
