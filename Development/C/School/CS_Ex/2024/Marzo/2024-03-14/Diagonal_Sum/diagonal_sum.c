36c99ef60d10f6a8fc9c2d8056819d2b5e81e9ad
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-03-15 10:45:46 +0100

    dd

--- Development/C/School/CS_Ex/2024/Marzo/2024-03-14/Diagonal_Sum/diagonal_sum.c
+++ Development/C/School/CS_Ex/2024/Marzo/2024-03-14/Diagonal_Sum/diagonal_sum.c
@@ -3,26 +3,33 @@ CONSEGNA
 --------------------------------------------------------------------------------------------------------------
 Data una matrice 9×9 verificare se la somma delle celle delle due diagonali è uguale.
 ------------------------------------------------------------------------------------------------------------*/
-
+//ancora non funzionante
 // LIBRARY
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // MACROS
 #define rows 9
 #define column 9
+
+#define MIN 1
+#define MAX 9
 
 // GRAPHIC FUNCTIONS
 void welcome(void);
 void endl(void);
 
 // FUNCTION
-void fill_diagonal(int matrix[][column]);
+int fill_diagonal(int matrix[][column]);
 
 int main(void){
+    srand(time(NULL));
     welcome();
 
     int matrix[rows][column];
-    fill_diagonal(matrix);
+
+    printf("+\n+ Greatest Sum: %d\n", fill_diagonal(matrix));
 
     endl();
     return 0;
@@ -38,19 +45,42 @@ void endl(void){
     puts("+--------------------------------------------------------------------+");
 }
 
-void fill_diagonal(int matrix[][column]){
-    for(int i=0; i<column; i++){
+int fill_diagonal(int matrix[][column]){
+    int sum = 0, sum_2 = 0, counter = column-1;
+    int i, j;
+
+    for(i=0; i<column; i++){
         printf("+ ");
-        for(int j=0; j<rows; j++){
+        for(j=0; j<rows; j++){
+            matrix[i][j] = rand()%(MAX - MIN + 1) + MIN;
+
             if(j==i){
-                matrix[i][j] = 1;
+                sum += matrix[i][j];
+                printf("\033[1;31m%d  \033[0m", matrix[i][j]);
+            }
+            else if(j==counter){
+                counter --;
+                sum_2 += matrix[i][j];
                 printf("\033[1;31m%d  \033[0m", matrix[i][j]);
             }
             else{
-                matrix[i][j] = 0;
                 printf("%d  ", matrix[i][j]);
             }
         }
         puts("");
     }
+
+    printf("+ First sum: %d\n", sum);
+    printf("+ Second sum: %d\n", sum_2);
+
+    int greatest_sum;
+
+    if(sum > sum_2){
+        greatest_sum = sum;
+    }
+    else{
+        greatest_sum = sum_2;
+    }
+
+    return greatest_sum;
 }
