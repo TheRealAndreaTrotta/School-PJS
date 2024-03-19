9ce014243fdb74479a9548c29995efa051cc0bfc
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-03-19 11:40:10 +0100

    19 Marzo 2024

--- Development/C/School/CS_Ex/2024/Marzo/2024-03-19/Sum Position/sum_position.c
+++ Development/C/School/CS_Ex/2024/Marzo/2024-03-19/Sum Position/sum_position.c
@@ -1,18 +1,36 @@
 /*------------------------------------------------------------------------------------------------------------
 CONSEGNA
 --------------------------------------------------------------------------------------------------------------
+Data una matrice n*m, rimpire la locazione con n+m
 ------------------------------------------------------------------------------------------------------------*/
 
 // LIBRARY
 #include <stdio.h>
 
 // MACROS
-#define SZ 13
+#define ROWS 3
+#define COLUMN 3
+
+#define MAX 9
+#define MIN 1
 
 // FUNCTIONS
+void sum_position(int matrix[][COLUMN]);
 
 // PROGRAM
 int main(void){
+    int matrix[ROWS][COLUMN];
+    sum_position(matrix);
 
     return 0;
+}
+
+void sum_position(int matrix[][COLUMN]){
+    for(int i=0; i<ROWS; i++){
+        for(int j=0; j<COLUMN; j++){
+            matrix[i][j] = i+j;
+            printf("%d  ", matrix[i][j]);
+        }
+        puts("");
+    }
 }
