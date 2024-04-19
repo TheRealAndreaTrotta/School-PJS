6b65f8e42df8fbd1c5751e8045adb3a4849b074c
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-04-19 10:50:46 +0200

    ddss

--- Development/Python/2024_04_18/PRS/prs.py
+++ Development/Python/2024_04_18/PRS/prs.py
@@ -1,10 +1,13 @@
 import random
+import os
 
 def combat(ply_choice, cpu_choice):
-    if (ply_choice == 'paper' and cpu_choice == 'rock') or (ply_choice == 'rock' and cpu_choice == 'scissors') or (ply_choice == 'scissors' and cpu_choice == 'paper'):
+    if (ply_choice == 'rock' and cpu_choice == 'scissors') or \
+       (ply_choice == 'paper' and cpu_choice == 'rock') or \
+       (ply_choice == 'scissors' and cpu_choice == 'paper'):
         return 1
-    elif (ply_choice == 'paper' and cpu_choice == 'paper') or (ply_choice == 'rock' and cpu_choice == 'rock') or (ply_choice == 'scissors' and cpu_choice == 'scissors'):
-        return 2
+    elif ply_choice == cpu_choice:
+        return -1
     else:
         return 0
 
@@ -15,6 +18,14 @@ def combat_print(chosen_shape):
         print(paper)
     else:
         print(scissors)
+
+def combat_sentence(result):
+    if result == 1:
+        print("YOU WIN!")
+    elif result == 0:
+        print("YOU LOSE!")
+    else:
+        print("DRAW")
 
 rock = '''
     _______
@@ -44,25 +55,24 @@ scissors = '''
 '''
 
 shapes = ['rock', 'paper', 'scissors']
-
 play_again = 'y'
 
-while play_again.lower() == 'y' or 'yes':
+while play_again.lower() in ['y', 'yes']:
+    combat_result = 0
+
     ply_choice = input("Write your choice (Rock-Paper-Scissors): ").lower()
     cpu_choice = random.choice(shapes)
 
     combat_print(ply_choice)
+    print(f"Jarvis: I say {cpu_choice}!")
     combat_print(cpu_choice)
 
-    result = combat(ply_choice, cpu_choice)
-
-    if result == 1:
-        print("YOU WIN!")
-    elif result == 0:
-        print("YOU LOSE!")
-    else:
-        print("DRAW")
-
+    combat_result = combat(ply_choice, cpu_choice)
+    combat_sentence(combat_result)
+    
     play_again = input("Do you want to play again? (yes/no): ")
+    
+    if play_again in ['y', 'yes']:
+        os.system('cls' if os.name == 'nt' else 'clear') 
 
-print("Thanks for playing!")
+print("\nThanks for playing!")
