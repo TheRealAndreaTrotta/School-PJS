ad60d5dd83a5b570bc99f1925ba4fee21b2050e0
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-05-02 13:27:47 +0200

    ss

---
+++ Development/C/School/Systems/IP/ip.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main(void) {
+
+    // IP_0 = 192.168.1.1; IP_1 = 192.168.1.5;  
+    uint32_t ip_0 = (192u * 256u * 256u * 256u) + (168u * 256u * 256u) + (1u * 256u) + 1u;
+    uint32_t ip_1 = (192u * 256u * 256u * 256u) + (168u * 256u * 256u) + (1u * 256u) + 5u;
+    
+    //printf("%u\n", ip_0); // IP0 convertito in intero decimale
+    //printf("%u\n", ip_1); // IP1 convertito in intero decimale
+
+    int difference = ip_1 - ip_0 + 1;   // Aggiungo uno per includere l'indirizzo in se
+
+    printf("The differenze between IP::192.168.1.1 and IP::192.168.1.5 is %d\n", difference);
+
+    return 0;
+}
