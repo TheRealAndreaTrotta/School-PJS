fa2f48113f8a8187ba2b4c1f6fd9442315068b67
TheRealAndreaTrotta <andreatrotta004@gmail.com>
2024-05-08 11:28:47 +0200

    linker

---
+++ Development/C/School/Systems/Systems_C/2024-05-07/mylib.c
@@ -0,0 +1,5 @@
+#include "mylib.h"
+
+int absolute(int number){
+    return (number >= 0? number : -1 *number); // operatore ternario
+}
---
+++ Development/Python/Code Wars/Reverse every other word in the string/main.py
@@ -0,0 +1,4 @@
+string = 'ciao, come stai?'
+reversed_string = string[::-1]
+
+print(reversed_string)
